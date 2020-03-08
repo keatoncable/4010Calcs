ea01d5b7dd09ee8f0032058ad49bd12d1cf0697a
Trustmeirussian <61611201+Trustmeirussian@users.noreply.github.com>
2020-03-08 09:15:06 -0400

    Minor changes
    I forgot what I changed

--- SolveTorque.m
+++ SolveTorque.m
@@ -1,16 +1,17 @@
-function [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D)
+function [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D,vf,surfacearea)
 mt=Lbs*0.453592; %mass in kg
 Load=mt*9.807; %weight in newtons
 r=(D/2)*0.0254; %meters
 
+%Drag Force
+CD=2.1; %drag coefficient for a box
+rho=1.225; %density of air at STP [kg/m^3]
+Fd=0.5*rho*vf^2*CD*surfacearea;
 
 
-
-
-
-A=[1,-Cr;0,1];
-B=[mt*a+Load*sin(ThetaR);Load*cos(ThetaR)];
-C=A\B; %normalforce and torque (Nm)
+A=[1,-Cr;0,1]; 
+B=[mt*a+Load*sin(ThetaR)+Fd;Load*cos(ThetaR)];
+C=A\B; % normalforce and torque (Nm)
 Fw=C(1);
 N=C(2);
 Torque=C(1)*r;
--- Wheel_Torque.m
+++ Wheel_Torque.m
@@ -7,6 +7,11 @@ ThetaD=15; ThetaR=deg2rad(ThetaD);
 Lbs=300;%weight in lbs
 % mt=Lbs*0.453592; %mass in kg
 % Load=mt*9.807; %weight in newtons
+width=12; %width of cart in inches
+width= width*0.0254; %width from inches to meters
+height=12; %height of the bucket in inches
+height= height*0.0254; %hieght from inches to meters
+surfacearea=width*height; %Surface area of the front of the cart
 %% Wheel Variables
 D=10; %Wheel diameter inches
 r=(D/2)*0.0254; %meters
@@ -20,44 +25,48 @@ Vf=Vf*0.44704; %m/s
 t=5; %time to reach final velocity (s)
 a=(Vf-Vi)/t; %m/s^2
 alpha=a/r; %radians/s^2
+%% Solve general solution
+[Fw,N,Torque]=SolveTorque(Cr,Lbs,a,ThetaR,D,Vf,surfacearea);
+VaraiblesUsed={'Rolling Coefficient';'Cart load [lbs.]';'Cart acceleration [m/s^2]';'Angle [deg.]';'Wheel diameter [inches]';'Final velocity [mph]'};
+VariableValues=[Cr;Lbs;a;ThetaD;D;Vf/0.44704];
+Table=table(VaraiblesUsed,VariableValues)
+fprintf("\nForce at wheel: %0.0f N\n", Fw)
+fprintf("Normal Force: %0.2f N\n", N)
+fprintf("Torque: %0.2f Nm\n", Torque)
+fprintf("Force at wheel: %0.2f lbf\n\n\n", Fw*0.22480894387096)
+
 %% Solve Load vs. Torque
-X1=linspace(1,Lbs,1000);
-Y1=X1;
-for i=1:length(X1)
-    Lbs=X1(i);
-    [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D);
-    Y1(i)=Torque;
-end
-subplot(3,1,1)
-plot(X1,Y1), xlabel('Load [lbs.]'), ylabel('Torque [Nm]')
+% X1=linspace(1,Lbs,1000);
+% Y1=X1;
+% for i=1:length(X1)
+%     Lbs=X1(i);
+%     [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D,Vf,surfacearea);
+%     Y1(i)=Torque;
+% end
+% subplot(2,2,1)
+% plot(X1,Y1), xlabel('Load [lbs.]'), ylabel('Torque [Nm]')
 %% Solve Incline vs Torque
 X2=linspace(0,ThetaD,1000);
-Y2=X2;
-for i=1:length(X2)
-    ThetaR=deg2rad(X2(i));
-    [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D);
-    Y2(i)=Torque;
+VMass=[50,100,150,200]; %variations of mass in kg
+Y2=zeros([length(VMass) length(X2)]);
+for j=1:length(VMass)
+    Lbs=VMass(j)/0.453592;
+    for i=1:length(X2)
+        ThetaR=deg2rad(X2(i));
+        [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D,Vf,surfacearea);
+        Y2(j,i)=Torque;
+    end
 end
-subplot(3,1,2)
+% subplot(2,2,2)
 plot(X2,Y2), xlabel('Incline [degrees]'), ylabel('Torque [Nm]')
 %% Solve Cr vs Torque
-Cr=0.3;
-X3=linspace(0,Cr,1000);
-Y3=X2;
-for i=1:length(X3)
-    Cr=(X3(i));
-    [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D);
-    Y3(i)=Torque;
-end
-subplot(3,1,3)
-plot(X3,Y3), xlabel('Coefficient of rolling resistance [-]'), ylabel('Torque [Nm]')
-
-
-% fprintf("Force at wheel: %0.0f N\n", Fw)
-% fprintf("Normal Force: %0.2f N\n", N)
-% fprintf("Torque: %0.2f Nm\n", Torque)
-% fprintf("Force at wheel: %0.2f lbf\n\n\n", Fw*0.22480894387096)
-
-%%Drag Force
-% 0.5*rho*velocity^2*CD*A
-% CD=2.1
+% Cr=0.3;
+% X3=linspace(0,Cr,1000);
+% Y3=X2;
+% for i=1:length(X3)
+%     Cr=(X3(i));
+%     [Fw,N,Torque] = SolveTorque(Cr,Lbs,a,ThetaR,D,Vf,surfacearea);
+%     Y3(i)=Torque;
+% end
+% subplot(2,2,3)
+% plot(X3,Y3), xlabel('Coefficient of rolling resistance [-]'), ylabel('Torque [Nm]')
